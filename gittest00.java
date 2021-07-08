ec7ba79c5a68f9a4c98c33889eba02d693a1477c
wenda yan <1161647633@qq.com>
2021-07-09 00:19:46 +0800

    dev version

--- gittest00.java
+++ gittest00.java
@@ -2,6 +2,6 @@ package gittest1;
 
 public class gittest00 {
     public static void main(String args[]){
-        System.out.println("Hi World");
+        System.out.println("Hi Worldssss");
     }
 }
