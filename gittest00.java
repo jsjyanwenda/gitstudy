1a57783e436862fd86527453235f127ae60f9035
wenda yan <1161647633@qq.com>
2021-07-09 00:27:11 +0800

    dsasd

--- gittest00.java
+++ gittest00.java
@@ -1,7 +1,7 @@
 package gittest1;
 
 public class gittest00 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Hi Worldssss");
     }
 }
